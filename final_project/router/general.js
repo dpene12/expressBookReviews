c96452b3d1aaed58ca0e5f819450f447dfac6110
Declan Pene <dpene12@berkeley.edu>
2024-04-04 23:26:45 -0400

    done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,35 +18,59 @@ public_users.post("/register", (req,res) => {
       return res.status(201).json({ message: "User registered successfully" });
 });
 
+const getBooklist = () => {
+    return new Promise((resolve, reject) => {
+        resolve(books);
+    });
+};
+
+const booklistISBN = (isbn) => {
+    return new Promise((resolve, reject) => {
+        let newisbn = parseInt(isbn);
+        if (books[newisbn]) {
+            resolve(books[newisbn]);
+        } else {
+            reject({ status: 404, message: "ISBN: "+ newisbn + " is not found" });
+        }
+    });
+};
+
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(books,null,4))
+public_users.get('/',async function (req, res) {
+    try {
+        const bookList = await getBooklist(); 
+        res.send(JSON.stringify(bookList,null,4));
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error getting list" });
+      }
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = req.params.isbn;
-  res.send(books[isbn])
+  booklistISBN(isbn).then(
+    result => res.send(result),
+    error => res.status(error.status).json({message: error.message})
+  )
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   const author = req.params.author;
-  for(let i = 1; i < 11; i++) {
-    if (books[i].author == author){
-        res.send(books[i])
-    }
-  }
+  getBooklist()
+    .then((bookList) => Object.values(bookList))
+    .then((books) => books.filter((book) => book.author === author))
+    .then((filteredBooks) => res.send(filteredBooks));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
-    for(let i = 1; i < 11; i++) {
-      if (books[i].title == title){
-          res.send(books[i])
-      }
-    }
+    getBooklist()
+    .then((bookList) => Object.values(bookList))
+    .then((books) => books.filter((book) => book.title === title))
+    .then((filteredBooks) => res.send(filteredBooks));
 });
 
 //  Get book review
